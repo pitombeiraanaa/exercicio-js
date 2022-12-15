8402921232ad241a424c01363e9ca98994145793
Ana  Alice Pitombeira <90655715+pitombeiraanaa@users.noreply.github.com>
2022-12-15 02:00:11 -0300

    Exercício 01 JavaScript

---
+++ script.js
@@ -0,0 +1,30 @@
+let nome = prompt('Digite seu nome: ')
+let idade = parseInt(prompt('Digite a sua idade: '))
+
+// Se o nome começar com J
+if (nome.toUpperCase().startsWith('J') && nome.toUpperCase().endsWith('A')) {
+  document.write(`Seu nome é ${nome} começa com a letra "J" e 
+   termina com a letra "A". A sua idade é ${idade} anos. <br> 
+    <br>`)
+} else if (nome.toUpperCase().startsWith('J')) {
+  document.write(
+    `Seu nome é ${nome} e começa coma letra "J". A sua idade é ${idade} anos. <br> <br>`
+  )
+
+  // Se o nome terminar com A
+} else if (nome.toLocaleUpperCase().endsWith('A')) {
+  document.write(`Seu nome é ${nome} e termina com a letra 
+   "A". A sua idade é ${idade} anos. <br> <br>`)
+
+  // Se o nome não começar com J ou terminar com A
+} else {
+  document.write(
+    `Seu nome é ${nome} e não começa com a letra "J" nem termina com letra "A" e a sua idade é ${idade} anos. <br> <br>`
+  )
+}
+
+// Enquanto a váriavel idade for menor que 60 criar um laço para contar de 2 em 2 até chegar em 60
+
+for (let contador = idade; contador <= 60; contador += 2) {
+  document.write(contador + ' ')
+}
